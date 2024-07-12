1b4e61244f03e50a5d5935f4390e8479aa07f907
Abreham Kassa <abrehamkassa19@gmail.com>
2024-07-12 11:52:54 +0300

    Initialization of the backend server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import config from '../config/config'
+import app from './express'
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log('Server started on port $s.', config.port);
+})
